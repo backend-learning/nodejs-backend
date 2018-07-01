856af1c099aa3ef97553c4a43e9f1cf5cd524067
Chongfa <cfachen@thoughtworks.com>
2018-07-02 00:16:44 +0800

    afaren - add get /heroes

--- src/server.js
+++ src/server.js
@@ -7,6 +7,24 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extend: true}));
 app.use(bodyParser.json());
 
+const HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+app.get('/api/heroes', (req, res)=> {
+    res.send(HEROES);
+});
+
+
 app.get('/query', (req, res)=> {
     res.send(req.query.name);
 });
